ef0e5cb0d6ca8dfe849e3793eba660dd6032d1ba
AbdulrahmanKaltham <abdlolrhman999@gmail.com>
2026-04-11 17:38:06 +0300

    fix env

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
-
-export const supabase = supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null;
+export const supabase = createClient(
+  'https://lxoelfdzrcvwccmkvkpu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4b2VsZmR6cmN2d2NjbWt2a3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTc3ODIsImV4cCI6MjA5MTM5Mzc4Mn0.TIx5gF5rmd__W0x0eqcTDrZxbyfmRbMrQ2FLZj9t9xU'
+);
